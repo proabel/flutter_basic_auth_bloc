97a78a5239c0aca9eebc4fb8837921228808c760
Abel D <proabel.d@gmail.com>
2020-05-25 19:28:48 +0530

    adding bloc builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:basic_bloc_auth/bloc/auth_bloc.dart';
+import 'package:basic_bloc_auth/repositoties/firebase-auth-repo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import './pages/login-page.dart';
 import './pages/home-page.dart';
 void main() {
-  runApp(MyApp());
+  final FirebaseAuthRepo _authRepository = FirebaseAuthRepo();
+  runApp(
+    MultiProvider(
+      providers: [
+        BlocProvider(create: (context) => AuthBloc(_authRepository),)
+      ],
+      child: MyApp()
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/pages/login-page.dart
+++ lib/pages/login-page.dart
@@ -1,4 +1,6 @@
+import 'package:basic_bloc_auth/bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key key}) : super(key: key);
@@ -6,7 +8,56 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Login Page'),
+      child: Scaffold(
+        body: BlocBuilder<AuthBloc, AuthState>(builder: (context, state){
+          if(state is AuthInitial){
+            return _buildLoginLayout(context);
+          }else if(state is LoggingIn){
+            return _buildSpinner();
+          }else if(state is LoginSuccess){
+            Navigator.pushReplacementNamed(context, '/home');
+          }
+        }),
+      )
+    );
+  }
+
+  Widget _buildLoginLayout(context){
+    final TextEditingController emailCtlr = TextEditingController();
+    final TextEditingController passwordCtlr = TextEditingController();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text('Login to app'),
+          const SizedBox(height: 20),
+          TextField(
+            controller: emailCtlr,
+            decoration: InputDecoration(
+              labelText: 'Email'   
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: passwordCtlr,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',   
+            ),
+          ),
+          const SizedBox(height: 20),
+          RaisedButton(
+            onPressed: (){},
+            child: Text('Login')
+          )
+        ],
+      )
+    );
+  }
+
+  Widget _buildSpinner(){
+    return Center(
+      child: CircularProgressIndicator()
     );
   }
 }
